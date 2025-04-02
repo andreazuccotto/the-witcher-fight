4b1b7c55c9428cba2efc66624322890c99fecb41
andreazuccotto <best_spartan@hotmail.it>
2025-04-02 15:04:56 +0200

    fix link

--- the-witcher-fight-app/src/app/page.js
+++ the-witcher-fight-app/src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
         <h1 className="text-center">Benvenuto in The Witcher Fight App</h1>
         <div className="text-center mt-4">
           <a href="/login" className="btn btn-primary mx-2">Login</a>
-          <a href="/register" className="btn btn-secondary mx-2">Registrati</a>
+          <a href="/registrazione" className="btn btn-secondary mx-2">Registrati</a>
         </div>
       </div>
     )
